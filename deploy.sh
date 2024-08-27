40e0ee1201a8eae16f00d2a1fb63bb5134401cb5
Thowbik Dustan <beejascaptain@gmail.com>
2024-08-27 22:44:07 +0100

    Adding deployment action file

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 sudo docker-compose kill
-sudo cosign verify --key /home/dustan/cosign.pub thowbikdustan/distroless-flask
+sudo cosign verify --key /home/ubuntu/cosign.pub thowbikdustan/distroless-flask
 if [ $? -ne 0 ]; then exit 1; fi
-sudo cosign verify --key /home/dustan/cosign.pub thowbikdustan/distroless-nginx
+sudo cosign verify --key /home/ubuntu/cosign.pub thowbikdustan/distroless-nginx
 if [ $? -ne 0 ]; then exit 1; fi
 sudo docker-compose up -d
