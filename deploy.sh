34f229032c86a968fa8634123d8003285cec1910
Thowbik Dustan <beejascaptain@gmail.com>
2024-08-28 15:06:05 +0100

    Modifying deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
 sudo docker-compose kill
-sudo cosign verify --key /home/ubuntu/cosign.pub thowbikdustan/distroless-flask
-if [ $? -ne 0 ]; then exit 1; fi
-sudo cosign verify --key /home/ubuntu/cosign.pub thowbikdustan/distroless-nginx
-if [ $? -ne 0 ]; then exit 1; fi
+cat docker-compose.yml | grep image | cut -d ":" -f 2 > images.txt
+IMAGE_LIST_FILE="images.txt"
+while IFS= read -r IMAGE_NAME; do
+    if [ -n $IMAGE_NAME ]; then
+        echo "verifying Image: $IMAGE_NAME"
+
+        cosign verify --key /home/ubuntu/cosign.pub $IMAGE_NAME
+        if [ $? -ne 0 ]; then echo "Image Verfication Failed" 
+            exit 1; fi
+    else
+        echo "Warning: Found empty line in image list file. Skipping..."
+    fi
+done < $IMAGE_LIST_FILE
 sudo docker-compose up -d
